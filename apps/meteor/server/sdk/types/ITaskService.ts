d14989091dcc1261e534f14e1f69e211e647340b
Gso Soft <bqt.gso@outlook.com>
2022-08-03 15:59:39 +0700

    merge with RC + IAchievable.ts

--- apps/meteor/server/sdk/types/ITaskService.ts
+++ apps/meteor/server/sdk/types/ITaskService.ts
@@ -1,18 +1,18 @@
 import { IPaginationOptions, IQueryOptions } from '@rocket.chat/core-typings';
-import { AtLeastOne, ITask, PartialBy } from '@rocket.chat/core-typings/src/gso';
+import { AtLeastOne, IAchievable, PartialBy } from '@rocket.chat/core-typings/src/gso';
 import { Cursor } from 'mongodb';
 
-export type ITaskLean = Omit<ITask, '_id' | '_updatedAt'>;
+export type ITaskLean = Omit<IAchievable, '_id' | '_updatedAt'>;
 
-export type ITaskCreateParams = PartialBy<Omit<ITask, '_id' | '_updatedAt' | 'startDate' | 'assignedBy' | 'assignedTo'>, 'sortOrder'>;
+export type ITaskCreateParams = PartialBy<Omit<IAchievable, '_id' | '_updatedAt' | 'startDate' | 'assignedBy' | 'assignedTo'>, 'sortOrder'>;
 
 export type ITaskUpdateParams = AtLeastOne<ITaskLean>;
 
 export interface ITaskService {
-	create(params: ITaskCreateParams): Promise<ITask>;
+	create(params: ITaskCreateParams): Promise<IAchievable>;
 	createMany(tasks: ITaskCreateParams[]): Promise<void>;
-	list(paginationOptions?: IPaginationOptions, queryOptions?: IQueryOptions<ITask>): Cursor<ITask>;
-	update(taskId: ITask['_id'], params: ITaskUpdateParams): Promise<ITask>;
-	delete(taskId: ITask['_id']): Promise<void>;
-	getTask(taskId: ITask['_id']): Promise<ITask>;
+	list(paginationOptions?: IPaginationOptions, queryOptions?: IQueryOptions<IAchievable>): Cursor<IAchievable>;
+	update(taskId: IAchievable['_id'], params: ITaskUpdateParams): Promise<IAchievable>;
+	delete(taskId: IAchievable['_id']): Promise<void>;
+	getTask(taskId: IAchievable['_id']): Promise<IAchievable>;
 }
